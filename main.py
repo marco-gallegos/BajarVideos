6629462ea196e8a8e8df63ae2f0754cfee9962b6
Marco Gallegos <ma_galeza@hotmail.com>
2019-04-06 23:22:59 -0600

    update : uso de paraemtro directorio
    se valida el funcionamiento de el parametro -d

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import peewee
 import os
+import argparse
 # constantes
 extensiones = [
     ".mp4",
     ".mkv",
     ".avi"
 ]
-file_name = "out.txt"
-destination_dir = "convertidos"
 
+parser = argparse.ArgumentParser()
+# parser.add_argument("-v", "--verbose", help="Mostrar información de depuración", action="store_true")
+parser.add_argument("-d", "--directory", help="Ruta absoluta del directorio de trabajo")
+args = parser.parse_args()
+
+
+if not args.directory:
+    print("se necesita un directorio de trabajo argumento -d --directory")
+    exit()
+
+#variables iniciales
+file_name = str(f"{args.directory}\\out.txt")
+destination_dir = str(f"{args.directory}\\convertidos")
 
 for extension in extensiones:
-    # comand = os.system(str(f"dir /B *{extension}"))
-    comand = os.system(str(f"dir /B *{extension} > {file_name}"))
+    comand_string = str(f"dir /B {args.directory}\\*{extension} >> {file_name}")
+    comand = os.system(comand_string)
 
 file = open(file_name,"r")
 video_files = file.readlines()
@@ -24,6 +36,6 @@ os.system(str(f"mkdir {destination_dir}"))
 for video in video_files:
     video = video.replace("\n", "")
     destination_name = video[:-4]
-    os.system(str('ffmpeg -i "{}" -c:v libx265 -crf 30 "{}/{}.mp4"'.format(video, destination_dir, destination_name)))
+    os.system(str(f"ffmpeg -i \"{args.directory}\\{video}\" -c:v libx265 -crf 30 \"{destination_dir}\\{destination_name}.mp4\""))
 
 
